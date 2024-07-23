56b743f1cc13c3539a95517b1a713a468063bec9
MGHefny <mgenedhefny@gmail.com>
2024-07-23 03:34:16 +0300

    000

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,15 @@
+//first task
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
+//get function
 export function getLast() {
   return ' is okay';
 }
 
+//comination
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();

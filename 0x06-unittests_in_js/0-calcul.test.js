f6d4c6f80e790cce8a29e17c0b9eb664534e819a
MGHefny <mgenedhefny@gmail.com>
2024-09-20 19:39:43 +0300

    1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,3 +1,4 @@
+/* expect */
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,29 @@
+/* expect */
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+
+describe('calculateNumber', () => {
+  it('control adding two rounded numbers when type is SUM', () => {
+    assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
+    assert.equal(calculateNumber('SUM', -1.2, -3.7), -5);
+  });
+
+  it('control subtracting two rounded numbers when type is SUBTRACT', () => {
+    assert.equal(calculateNumber('SUBTRACT', 5.9, 2.3), 4);
+    assert.equal(calculateNumber('SUBTRACT', -5.9, -2.3), -4);
+  });
+
+  it('control dividing two rounded numbers when type is DIVIDE', () => {
+    assert.equal(calculateNumber('DIVIDE', 8, 2), 4);
+  });
+
+  it('returns Error when dividing by 0', () => {
+    assert.equal(calculateNumber('DIVIDE', 4, 0), 'Error');
+    assert.equal(calculateNumber('DIVIDE', -8, 0), 'Error');
+  });
+
+  it('throws an error when an invalid type is provided', () => {
+    assert.throws(() => calculateNumber('INVALID_TYPE', 1, 2), Error);
+  });
+});

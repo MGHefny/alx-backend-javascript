fb8840d155839ef3bbaa1526b605c8ad276c4bed
MGHefny <mgenedhefny@gmail.com>
2024-09-20 20:13:29 +0300

    4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,30 +1,21 @@
 /* expect */
 const sinon = require('sinon');
 const Utils = require('./utils');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  let theStu;
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const theSP = sinon.stub(Utils, 'calculateNumber');
 
-  beforeEach(() => {
-    theStu = sinon.theStu(Utils, 'calculateNumber').returns(10);
-  });
-
-  afterEach(() => {
-    theStu.restore();
-  });
-
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const theSP = sinon.spy(console, 'log');
-
+    theSP.returns(10);
     sendPaymentRequestToApi(100, 20);
-
-    sinon.assert.calledOnce(theStu);
-    sinon.assert.calledWith(theStu, 'SUM', 100, 20);
-
-    sinon.assert.calledOnce(theSP);
-    sinon.assert.calledWith(theSP, 'The total is: 10');
-
+    expect(theSP.calledWith('SUM', 100, 20)).to.be.true;
+    expect(theSP.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
     theSP.restore();
+    bigBrother.log.restore();
   });
 });
